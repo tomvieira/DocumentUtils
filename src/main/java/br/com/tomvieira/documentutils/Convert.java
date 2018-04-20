b9407230e3cf9c922d3c43e26d8be70ae8c2990b
Wellington Vieira <wellington.analista@gmail.com>
2018-04-20 16:41:05 -0300

    styles

--- src/main/java/br/com/tomvieira/documentutils/Convert.java
+++ src/main/java/br/com/tomvieira/documentutils/Convert.java
@@ -1,9 +1,10 @@
 package br.com.tomvieira.documentutils;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
 import org.docx4j.XmlUtils;
 import org.docx4j.convert.in.xhtml.XHTMLImporterImpl;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
@@ -15,11 +16,18 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 public class Convert {
 
     public static void htmlToDocx() throws Exception {
-        
+
         InputStream xhtml = new FileInputStream("xhtml/relatorio.xhtml");
+        InputStream css = new FileInputStream("xhtml/styles.css");
 
         WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage
                 .createPackage();
+        String body = IOUtils.toString(css, StandardCharsets.UTF_8.name());
+        //HtmlCssHelper.createDefaultCss(true, true, sb);
+
+        //FormattingOption option = 
+        //XHTMLImporter.setHyperlinkStyle("xhtml/styles.css");
+        wordMLPackage.getMainDocumentPart().getStyleDefinitionsPart().setCss(body);
         XHTMLImporterImpl XHTMLImporter = new XHTMLImporterImpl(wordMLPackage);
         wordMLPackage.getMainDocumentPart().getContent()
                 .addAll(XHTMLImporter.convert(xhtml, null));
